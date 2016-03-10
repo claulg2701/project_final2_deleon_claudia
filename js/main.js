7f9404f6a7de91dcbe18ead1b351c308cc0d7e4e
Claudia De Leon <claulg2701@gmail.com>
2016-03-10 18:21:14 -0500

    test resizing menu

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
-function loadMenu()
-{
+function loadMenu() {
     var myMenu = document.getElementById( 'nav-links' );
-
     var openCloseMenu = document.getElementById( 'menu-icon' );
-
     var logo = document.getElementById( 'logo' );
 
     if ( myMenu.className == 'open' ) {
@@ -20,5 +17,35 @@ function loadMenu()
       myMenu.style.transition = 'transform 0.3s ease';
       myMenu.style.zIndex = '1';
       logo.style.visibility = 'hidden';
+    }
+}
+
+window.onresize = function() {
+    var mq = window.matchMedia( '(min-device-width: 1024px)' );
+    var myMenu = document.getElementById( 'nav-links' );
+    var logo = document.getElementById( 'logo' );
+    if ( mq.matches ) {
+      console.log( 'if section' );
+      if ( myMenu.className === 'open' )
+      {
+          myMenu.style.transform = 'translateY(0)';
+          myMenu.style.zIndex = '0';
+          logo.style.visibility = 'visible';
+      }
+
+      if ( myMenu.className === 'close' || myMenu.className === '' )
+      {
+          myMenu.style.transform = 'translateY(-350)';
+          myMenu.style.zIndex = '0';
+          logo.style.visibility = 'visible';
+      }
+
+    } else {
+      if ( myMenu.className === 'open' )
+      {
+          myMenu.style.transform = 'translateY(-350)';
+          myMenu.style.zIndex = '0';
+          logo.style.visibility = 'visible';
+      }
     }
 }
