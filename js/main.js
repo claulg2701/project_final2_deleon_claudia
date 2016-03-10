9b82515fa532fb232a442d6deb7533b6d321799b
Claudia De Leon <claulg2701@gmail.com>
2016-03-10 17:15:24 -0500

    decorate menu links for mobile

--- js/main.js
+++ js/main.js
@@ -1,20 +1,24 @@
 function loadMenu()
 {
     var myMenu = document.getElementById( 'nav-links' );
+
     var openCloseMenu = document.getElementById( 'menu-icon' );
 
+    var logo = document.getElementById( 'logo' );
 
     if ( myMenu.className == 'open' ) {
       myMenu.className = '';
-      myMenu.style.transform = 'translateY(-300px)';
+      myMenu.style.transform = 'translateY(-350px)';
       myMenu.style.transition = 'transform 0.3s ease';
       openCloseMenu.className = 'fa fa-bars';
       myMenu.style.zIndex = '0';
+      logo.style.visibility = 'visible';
     } else {
       myMenu.className = 'open';
       openCloseMenu.className = 'fa fa-times';
-      myMenu.style.transform = 'translateY(-75px)';
+      myMenu.style.transform = 'translateY(-100px)';
       myMenu.style.transition = 'transform 0.3s ease';
       myMenu.style.zIndex = '1';
+      logo.style.visibility = 'hidden';
     }
 }
